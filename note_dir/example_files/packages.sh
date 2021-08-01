7bbe09296f336fd5c101f294bfa72b390bfe4136
kymb0 <55473161+kymb0@users.noreply.github.com>
2021-08-01 10:10:55 +0930

    Create packages.sh

---
+++ note_dir/example_files/packages.sh
@@ -0,0 +1,3 @@
+# Only to be run once
+
+sudo apt install open-vm-tools-desktop build-essential libc6-dev libncurses5-dev git gzip help2man tree bzip2 linux-tools-generic gperf flex bison make autoconf automake texinfo bc curl mtd-utils net-tools device-tree-compiler dosfstools liblzo2-dev libfuse-dev libelf-dev u-boot-tools gawk libgtk2.0-dev gparted parted screen patch qt4-default qt4-dev-tools squashfs-tools symlinks sysfsutils libssl-dev libglade2-dev libhugetlbfs-dev libtool-bin libpython-dev
